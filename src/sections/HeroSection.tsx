19dc36bc65a7f950621a8d8c80364e00aa13cfd7
Sergio Gachuz <sergioegch@hotmail.com>
2026-01-18 00:32:10 -0600

    Build(v1)

--- src/sections/HeroSection.tsx
+++ src/sections/HeroSection.tsx
@@ -34,7 +34,7 @@ export const HeroSection = () => {
             </div>
             <div className="mt-6 flex justify-center gap-3">
               <Button variant="link" className="group bg-black text-white shadow-sm transition-shadow hover:shadow hover:no-underline hover:cursor-pointer" asChild>
-                <a href="/Curriculum.pdf" target='_blank'>
+                <a href="./assets/Curriculum.pdf" target='_blank'>
                   Descargar CV{" "}
                   <Download className="ml-2 h-4 transition-transform group-hover:translate-x-0.5" />
                 </a>
