8bfc8ee548b75eec7c66cc77050055bbe346f67c
Sergio Gachuz <sergioegch@hotmail.com>
2026-09-09 22:52:15 -0600

    Build(v4)

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,9 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  build: {
+    outDir: 'docs',
+  },
   plugins: [
     react(),
     tailwindcss(),
